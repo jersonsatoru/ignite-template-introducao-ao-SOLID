25809427e379fa50af5befa25780dfa968892887
Jerson Uyekita <jerson.uyekita@gok.digital>
2021-06-13 20:34:33 -0300

    Add swagger-ui-express and list all, show and turn do admin docs

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
+import swaggerUi from "swagger-ui-express";
+
+import path from 'path'
+
 import { app } from ".";
+
+import Yaml from 'yamljs'
+
+const swaggerFile = Yaml.load(path.resolve(__dirname, 'swagger.yml'))
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
