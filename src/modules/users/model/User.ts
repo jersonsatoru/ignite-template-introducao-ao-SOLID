b8e3534b972aa64aafdfc47c8059ee3d42831b59
Jerson Uyekita <jerson.uyekita@gok.digital>
2021-06-13 12:50:32 -0300

    Add listUsers controller and useCase

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,12 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id: string;
+  name: string;
+  email: string;
+  admin: boolean;
+  created_at: Date;
+  updated_at: Date;
 }
 
 export { User };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,3 +1,5 @@
+import { response } from "express";
+
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -9,7 +11,10 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user || !user.admin) throw new Error("Unauthorized");
+
+    return this.usersRepository.list();
   }
 }
 
